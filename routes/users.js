3db4734973f2389b3c3d5c09b82c40fa45de21ef
kuixir <kuixir@uci.edu>
2020-01-25 15:31:42 -0800

    test commit

--- routes/users.js
+++ routes/users.js
@@ -8,18 +8,15 @@ router.post('/', function(req, res, next) {
   //res.send({ fulfillmentText: 'hello world' });
 
   if (req.body.intent.displayName === 'Default Welcome Intent') {
-
     res.send({ fulfillmentText: 'hello world' });
-
   } else {
-
     res.send({ fulfillmentText: 'hello world2' });
-  //   let r = `display name: ${req.body.intent.displayName}`;
-  //   let response = {
-  //     fulfillment_messages: [{ text: { text: [r] } }],
-  //   };
-  //   return res.send(response);
-  // }
+    //   let r = `display name: ${req.body.intent.displayName}`;
+    //   let response = {
+    //     fulfillment_messages: [{ text: { text: [r] } }],
+    //   };
+    //   return res.send(response);
+  }
 });
 
 module.exports = router;
