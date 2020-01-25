01d9e8cad315e31d605898432ac16acd53e45b20
kuixir <kuixir@uci.edu>
2020-01-25 15:02:22 -0800

    test commit

--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,17 @@ var router = express.Router();
 /* GET users listing. */
 router.post('/', function(req, res, next) {
   console.log(req.body);
+  if (req.body.intent.displayName === 'Default Welcome Intent') {
+    let response = {
+      fulfillment_text: 'hello world',
+    };
+    res.json(response);
+  } else {
+    let response = {
+      fulfillment_text: `display name: ${req.body.intent.displayName}`,
+    };
+    res.json(response);
+  }
 });
 
 module.exports = router;
