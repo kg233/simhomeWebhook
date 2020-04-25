6162c75cca22645baf2d95386a17bf698849375b
Sun Zhenhao <Zhenhaos@uci.edu>
2020-04-24 18:35:12 -0600

    Html and Js file for creating a chart

---
+++ Html_chart/data_file.js
@@ -0,0 +1,20 @@
+
+// Create data for graph
+
+
+// the device which you want to present just return a list 
+var device_list = ['TV','dell TV','lamp','alexa','light']
+// the corresponding spending of energy
+var energy_consumption = ['25','30','15','50','6']
+
+// type of chart you want
+var chart_type = 'bar'
+
+//Column color, which you can assign or randomly choose
+var Color_list = [
+    'rgba(255,99,132,0.6)',
+    'rgba(54,162,235,0.6)',
+    'rgba(255,206,86,0.6)',
+    'rgba(75,192,192,0.6)',
+    'rgba(153,102,255,0.6)',
+]
